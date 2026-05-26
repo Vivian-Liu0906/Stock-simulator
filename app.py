fd798b81e064b23e357431c89399bc3c2d787664
Vivian <vivi.liu0906@gmail.com>
2026-05-25 21:42:31 -0600

    接入yfinance，股票数据API完成

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/stock/<symbol>')
+def get_stock(symbol):
+    ticker = yf.Ticker(symbol)
+    info = ticker.info
+    return jsonify({
+        'name': info.get('longName', symbol),
+        'price': info.get('currentPrice', 'N/A'),
+        'change': info.get('regularMarketChangePercent', 'N/A'),
+        'high': info.get('dayHigh', 'N/A'),
+        'low': info.get('dayLow', 'N/A'),
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
